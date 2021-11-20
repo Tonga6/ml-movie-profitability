ac03b3e3d46897fe916d68a0589a29b083242f6f
Tonga6 <60393889+Tonga6@users.noreply.github.com>
2021-11-20 15:24:35 +0000

    added python file for tmdb api calls and json of 10000 movies

---
+++ tmdb_api_caller.py
@@ -0,0 +1,72 @@
+import requests
+import json
+api_key = "7ae9458e891125ad33f584367f999d05"
+
+movie_db = {
+    "results":[
+        
+    ]
+}
+template = {
+    "title":[],
+    "features":[{
+        "genre_ids":[],
+        "production_companies":[],
+        "release_date":[],
+        "vote_average":[],
+        "vote_count":[],
+        "budget":[],
+        "profit":[]
+    }
+    ]
+   
+}
+
+for i in range(1,501):
+
+    response = requests.get("https://api.themoviedb.org/3/discover/movie?api_key="+ api_key+"&page="+str(i))
+    result = response.json()
+
+    j = 0
+    for movie in result['results']:
+        template = {
+            "title":[],
+            "features":[{
+                "genre_ids":[],
+                "production_companies":[],
+                "release_date":[],
+                "vote_average":[],
+                "vote_count":[],
+                "budget":[],
+                "profit":[]
+            }]  
+        }
+
+        try:
+            movie_id = result['results'][j]['id']
+            movie_response = requests.get("https://api.themoviedb.org/3/movie/" + str(movie_id) +"?api_key="+ api_key+"&language=en-US")
+            movie_result = movie_response.json()
+            #movie_result = json.dumps(movie_result)
+            #print(movie_result)
+            #print(movie_result['adult'])
+            template['title'] = movie_result['title']
+            #template['features'][0]['genre_ids'] = movie_result['genre_ids']
+            template['features'][0]['release_date'] = movie_result['release_date']
+            template['features'][0]['production_companies'] = movie_result['production_companies']
+            template['features'][0]['vote_average'] = movie_result['vote_average']
+            template['features'][0]['vote_count'] = movie_result['vote_count']
+            template['features'][0]['budget'] = movie_result['budget']
+            template['features'][0]['profit'] = movie_result['revenue'] - movie_result['budget']
+
+            movie_db['results'].append(template)    
+        except KeyError:
+            print("KeyError")
+        j+=1
+
+
+db=json.dumps(movie_db, indent=4)
+with open("movie_db.json", "w") as outfile:
+    outfile.write(db)
+
+
+
