2c0b8c3ed889bc14ad5718c95579f6f7d6473b73
Tonga6 <60393889+Tonga6@users.noreply.github.com>
2021-11-25 19:50:29 +0000

    added json of labels, removed movies with 0 budget

--- tmdb_api_caller.py
+++ tmdb_api_caller.py
@@ -7,19 +7,8 @@ movie_db = {
         
     ]
 }
-template = {
-    "title":[],
-    "features":[{
-        "genre_ids":[],
-        "production_companies":[],
-        "release_date":[],
-        "vote_average":[],
-        "vote_count":[],
-        "budget":[],
-        "profit":[]
-    }
-    ]
-   
+labels_json = {
+    'labels':[]
 }
 
 for i in range(1,100):
@@ -38,7 +27,6 @@ for i in range(1,100):
                 "vote_average":[],
                 "vote_count":[],
                 "budget":[],
-                "profitable":[],
                 "star_popularity":[]
             }]  
         }
@@ -50,30 +38,35 @@ for i in range(1,100):
             #movie_result = json.dumps(movie_result)
             #print(movie_result)
             #print(movie_result['adult'])
-            template['title'] = movie_result['title']
-            template['features'][0]['genre_ids'] = movie_result['genres'][0]['id']
-            template['features'][0]['release_date'] = movie_result['release_date']
-            #for x in range(0,len(movie_result['production_companies'])):
-            template['features'][0]['production_companies'].append(movie_result['production_companies'][0]['id'])
-            template['features'][0]['vote_average'] = movie_result['vote_average']
-            template['features'][0]['vote_count'] = movie_result['vote_count']
-            template['features'][0]['budget'] = movie_result['budget']
-            template['features'][0]['profitable'] = movie_result['revenue'] - movie_result['budget'] > 0
+            if(movie_result['budget'] != 0):
+                template['title'] = movie_result['title']
+                template['features'][0]['genre_ids'] = movie_result['genres'][0]['id']
+                template['features'][0]['release_date'] = movie_result['release_date']
+                #for x in range(0,len(movie_result['production_companies'])):
+                template['features'][0]['production_companies'].append(movie_result['production_companies'][0]['id'])
+                template['features'][0]['vote_average'] = movie_result['vote_average']
+                template['features'][0]['vote_count'] = movie_result['vote_count']
+                template['features'][0]['budget'] = movie_result['budget']
+                labels_json['labels'].append(movie_result['revenue'] - movie_result['budget'] > 0)
 
 
-            credit_response = requests.get("https://api.themoviedb.org/3/movie/" + str(movie_id) +"/credits?api_key="+api_key+"&language=en-US")
-            credit_result = credit_response.json()
-            # movie_result = json.dumps(credit_result)
-            # print(movie_result)
-            template['features'][0]['star_popularity']= credit_result['cast'][0]['popularity']
+                credit_response = requests.get("https://api.themoviedb.org/3/movie/" + str(movie_id) +"/credits?api_key="+api_key+"&language=en-US")
+                credit_result = credit_response.json()
+                # movie_result = json.dumps(credit_result)
+                # print(movie_result)
+                template['features'][0]['star_popularity']= credit_result['cast'][0]['popularity']
 
-            movie_db['results'].append(template)    
+                movie_db['results'].append(template)    
         except KeyError:
             print("KeyError")
         except IndexError:
             print("IndexError")
         j+=1
 
+
+labels_json=json.dumps(labels_json, indent=4)
+with open("labels.json", "w") as outfile:
+    outfile.write(labels_json)
 
 db=json.dumps(movie_db, indent=4)
 with open("movie_db.json", "w") as outfile:
