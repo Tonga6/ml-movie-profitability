27ef288352a5634c35b5e13040d6b9b047bebe59
Tonga6 <60393889+Tonga6@users.noreply.github.com>
2021-11-25 09:17:45 +0000

    added main actor popularity and cleaned prod_comp

--- tmdb_api_caller.py
+++ tmdb_api_caller.py
@@ -22,9 +22,9 @@ template = {
    
 }
 
-for i in range(1,501):
+for i in range(1,2):
 
-    response = requests.get("https://api.themoviedb.org/3/discover/movie?api_key="+ api_key+"&page="+str(i))
+    response = requests.get("https://api.themoviedb.org/3/discover/movie?api_key="+ api_key+"&include_adult=false"+"&page="+str(i))
     result = response.json()
 
     j = 0
@@ -38,7 +38,8 @@ for i in range(1,501):
                 "vote_average":[],
                 "vote_count":[],
                 "budget":[],
-                "profit":[]
+                "profit":[],
+                "star_popularity":[]
             }]  
         }
 
@@ -52,11 +53,20 @@ for i in range(1,501):
             template['title'] = movie_result['title']
             #template['features'][0]['genre_ids'] = movie_result['genre_ids']
             template['features'][0]['release_date'] = movie_result['release_date']
-            template['features'][0]['production_companies'] = movie_result['production_companies']
+            print(range(0,len(movie_result['production_companies'])))
+            for x in range(0,len(movie_result['production_companies'])):
+                template['features'][0]['production_companies'].append(movie_result['production_companies'][x]['id'])
             template['features'][0]['vote_average'] = movie_result['vote_average']
             template['features'][0]['vote_count'] = movie_result['vote_count']
             template['features'][0]['budget'] = movie_result['budget']
             template['features'][0]['profit'] = movie_result['revenue'] - movie_result['budget']
+
+
+            credit_response = requests.get("https://api.themoviedb.org/3/movie/" + str(movie_id) +"/credits?api_key="+api_key+"&language=en-US")
+            credit_result = credit_response.json()
+            # movie_result = json.dumps(credit_result)
+            # print(movie_result)
+            template['features'][0]['star_popularity']= credit_result['cast'][0]['popularity']
 
             movie_db['results'].append(template)    
         except KeyError:
